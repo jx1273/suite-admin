5de289818c5be212b7fa65c21a9199f427840786
whbbit1999 <luckyboy_dong@sina.com>
2025-01-01 10:58:57 +0800

    feat: App.vue add Suspense and fallback loading component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Loading from '@/components/Loading/index.vue'
 import Toaster from '@/components/ui/toast/Toaster.vue'
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 </script>
@@ -7,7 +8,13 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
   <Toaster />
   <VueQueryDevtools />
 
-  <router-view v-slot="{ Component, route }">
-    <component :is="Component" :key="route" />
-  </router-view>
+  <Suspense>
+    <router-view v-slot="{ Component, route }">
+      <component :is="Component" :key="route" />
+    </router-view>
+
+    <template #fallback>
+      <Loading />
+    </template>
+  </Suspense>
 </template>
