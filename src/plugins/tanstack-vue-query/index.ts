6c0065faa63f0b3075a15d76520e37a3c6cb777c
whbbit1999 <luckyboy_dong@sina.com>
2025-01-04 03:19:21 +0800

    feat(plugin): add tanstack-vue-query

--- src/plugins/tanstack-vue-query/index.ts
+++ src/plugins/tanstack-vue-query/index.ts
@@ -1,8 +1,17 @@
 import type { App } from 'vue'
-import { VueQueryPlugin } from '@tanstack/vue-query'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 
 export function setupTanstackVueQuery(app: App) {
   app.use(VueQueryPlugin, {
     enableDevtoolsV6Plugin: true,
+    queryClient,
   })
 }
