571dd4a1073ddd5ef7919f23fd4898981b201510
whbbit1999 <luckyboy_dong@sina.com>
2025-02-10 10:42:36 +0800

    feat: added a example module to talk to ai

---
+++ src/pages/ai-talk/components/TalkItem.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { cn } from '@/lib/utils'
+import Copy from './Copy.vue'
+import Refresh from './Refresh.vue'
+import RobotAvatar from './RobotAvatar.vue'
+import ThumbDown from './ThumbDown.vue'
+import ThumbUp from './ThumbUp.vue'
+
+type TalkType = 'self' | 'system'
+interface Props {
+  content: string
+  type: TalkType
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    :class="cn(
+      'text-popover-foreground w-fit flex',
+      type === 'self' ? 'text-right ml-auto' : 'text-left mr-auto',
+    )"
+  >
+    <RobotAvatar v-if="type !== 'self'" class="mt-2 mr-2" />
+    <div>
+      <p class="p-4 rounded-lg bg-muted">
+        {{ content }}
+      </p>
+      <div v-if="type !== 'self'">
+        <div class="flex items-center gap-2 mt-2">
+          <Copy :content="content" />
+          <Refresh />
+          <ThumbUp />
+          <ThumbDown />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/ai-talk/components/TalkList.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import TalkItem from './TalkItem.vue'
+
+interface Props {
+  talks: {
+    type: string
+    text: string
+  }[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="">
+    <TalkItem
+      v-for="talk in talks" :key="talk.text"
+      :content="talk.text"
+      :type="talk.type"
+      class="mb-2"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
