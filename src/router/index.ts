051ec8a3c67baaa0797a3928bfdbba0e9d38e5ee
whbbit1999 <luckyboy_dong@sina.com>
2025-05-07 09:50:34 +0800

    fix: route mismatch redirects to page not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,12 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router'
 import generatedRoutes from '~pages'
 import { createRouterGuard } from './guard'
+import publicRoutes from './public-routes'
 
 const routes = setupLayouts(generatedRoutes)
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [...routes, ...publicRoutes],
   scrollBehavior() {
     return { left: 0, top: 0, behavior: 'smooth' }
   },
